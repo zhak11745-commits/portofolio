01019718489ec5becd77a9e9f800ad537cfc4628
Fanzxml <zhak11745@gmail.com>
2025-10-07 13:22:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Menggunakan 'const' huruf kecil dan kutip tunggal
 // Mengambil elemen tombol dari HTML yang memiliki ID 'sapaButton'
 const sapaButton = document.getElementById('sapaButton');
 
 // Menambahkan event listener. Ketika tombol diklik, fungsi di dalamnya akan dijalankan.
 sapaButton.addEventListener('click', function() {
   // Menampilkan kotak pesan alert
-  alert('Halo! terimakasih sudah berkunjung!');
+  alert('Halo! Terima kasih sudah berkunjung!'); // Perbaikan pada penulisan "Terima kasih" dan huruf kapital
 });
